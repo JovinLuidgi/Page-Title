633147dd80b2d031f6dffef4abb6c770f3e7f9ea
JovinLuidgi <43542508+JovinLuidgi@users.noreply.github.com>
2019-02-11 08:58:14 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,69 @@
+$('#fortniteimg').click(function() {
+    $('body').css('background-color','rgb(167, 104, 204)');
+    $('#forhonorimg').remove();
+    $('#nbaimg').remove();
+    $('#lolimg').remove();
+    $('#codimg').remove();
+    $('body').append('<h1 class="seven30"><a href="https://www.epicgames.com/fortnite/en-US/patch-notes/v7-30-content-update" target="_blank">Content Update 7.30</a></h1>');
+    $('body').append('<h1 class="seven20"><a href="https://www.epicgames.com/fortnite/en-US/patch-notes/v7-20" target="_blank">Patch Notes 7.20</a></h1>');
+    $('body').append('<h1 class="seven20"><a href="https://www.epicgames.com/fortnite/en-US/patch-notes/v7-20-content-update" target="_blank">Content Update 7.20</a></h1>');
+    $('body').append('<h1 class="seven10"><a href="https://www.epicgames.com/fortnite/en-US/patch-notes/v7-10" target="_blank">Patch Notes 7.10</a></h1>');
+    $('body').append('<h1 class="seven10"><a href="https://www.epicgames.com/fortnite/en-US/patch-notes/v7-10-content-update-1" target="_blank">Content Update #1 7.10</a></h1>');
+    $('body').append('<h1 class="seven10"><a href="https://www.epicgames.com/fortnite/en-US/patch-notes/v7-10-content-update-2" target="_blank">Content Update #2 7.10</a></h1>');
+    $('body').append('<h1 class="seven10"><a href="https://www.epicgames.com/fortnite/en-US/patch-notes/v7-10-content-update-3" target="_blank">Content Update #3 7.10</a></h1>');
+    $("body").append('<button class="button">Home</button>'); 
+});
+$('#forhonorimg').click(function() {
+    $('body').css('background-color','rgb(167, 104, 204)');
+    $('#fortniteimg').remove();
+    $('#nbaimg').remove();
+    $('#lolimg').remove();
+    $('#codimg').remove();
+    $('body').append('<h1 class=""><a href="https://forhonor.ubisoft.com/game/en-us/news-community/152-342840-16/patch-notes-v2040" target="_blank">Patch Notes v2.04.0</a></h1>');
+    $('body').append('<h1 class=""><a href="https://forhonor.ubisoft.com/game/en-us/news-community/152-342137-16/live-update-2031" target="_blank">Patch Notes v2.03.1</a></h1>');
+    $('body').append('<h1 class=""><a href="https://forhonor.ubisoft.com/game/en-us/news-community/152-341589-16/patch-notes-v2030" target="_blank">Patch Notes v2.03.0</a></h1>');
+    $('body').append('<h1 class=""><a href="https://forhonor.ubisoft.com/game/en-us/news-community/152-341101-16/patch-v2023" target="_blank">Patch Notes v2.02.3</a></h1>');
+    $('body').append('<h1 class=""><a href="https://forhonor.ubisoft.com/game/en-us/news-community/152-340206-16/patch-v2022" target="_blank">Patch Notes v2.02.2</a></h1>');
+    $("body").append('<button class="button">Home</button>');
+});
+$('#nbaimg').click(function() {
+    $('body').css('background-color','rgb(167, 104, 204)');
+    $('#forhonorimg').remove();
+    $('#fortniteimg').remove();
+    $('#lolimg').remove();
+    $('#codimg').remove();
+    $('body').append('<h1 class=""><a href="https://support.2k.com/hc/en-us/articles/360016377854-Patch-Notes-Jan-29-2019" target="_blank">Patch Notes:Jan. 29, 2019</a></h1>');
+    $('body').append('<h1 class=""><a href="https://support.2k.com/hc/en-us/articles/360013251013-Patch-Notes-Dec-5-2018" target="_blank">Patch Notes:Jan. 29, 2019</a></h1>');
+    $('body').append('<h1 class=""><a href="https://support.2k.com/hc/en-us/articles/360013251013-Patch-Notes-Dec-5-2018" target="_blank">Patch Notes:Dec. 5, 2018</a></h1>');
+    $('body').append('<h1 class=""><a href="https://support.2k.com/hc/en-us/articles/360012297974-Patch-Notes-Nov-19-2018" target="_blank">Patch Notes:Nov. 9, 2018</a></h1>');
+    $('body').append('<h1 class=""><a href="https://support.2k.com/hc/en-us/articles/360011481533-Patch-Notes-Oct-30-2018" target="_blank">Patch Notes:Oct. 30, 2018</a></h1>');
+    $("body").append('<button class="button">Home</button>');
+});
+$('#lolimg').click(function() {
+    $('body').css('background-color','rgb(167, 104, 204)');
+    $('#forhonorimg').remove();
+    $('#fortniteimg').remove();
+    $('#nbaimg').remove();
+    $('#codimg').remove();
+    $('body').append('<h1 class=""><a href="https://na.leagueoflegends.com/en/news/game-updates/patch/patch-93-notes" target="_blank">Patch Notes 9.3</a></h1>');
+    $('body').append('<h1 class=""><a href="https://na.leagueoflegends.com/en/news/game-updates/patch/patch-92-notes" target="_blank">Patch Notes 9.2</a></h1>');
+    $('body').append('<h1 class=""><a href="https://na.leagueoflegends.com/en/news/game-updates/patch/patch-91-notes" target="_blank">Patch Notes 9.1</a></h1>');
+    $('body').append('<h1 class=""><a href="https://na.leagueoflegends.com/en/news/game-updates/patch/patch-824b-notes" target="_blank">Patch Notes 8.24b</a></h1>');
+    $('body').append('<h1 class=""><a href="https://na.leagueoflegends.com/en/news/game-updates/patch/patch-824-notes" target="_blank">Patch Notes 8.24</a></h1>');
+    $("body").append('<button class="button">Home</button>');
+});
+$('#codimg').click(function() {
+    $('body').css('background-color','rgb(167, 104, 204)');
+    $('#forhonorimg').remove();
+    $('#fortniteimg').remove();
+    $('#nbaimg').remove();
+    $('#lolimg').remove();
+    $('h1').text('CoD Black Ops 4');
+    $('body').append('<h1 class=""><a href="https://community.callofduty.com/t5/Call-of-Duty-Black-Ops-4-PC/Call-of-Duty-Black-Ops-4-PC-Updates-1-17-2019/td-p/10877547" target="_blank">All Patch Notes (PC)</a></h1>');
+    $('body').append('<h1 class=""><a href="https://community.callofduty.com/t5/Call-of-Duty-Black-Ops-4-Xbox/Call-of-Duty-Black-Ops-4-XB1-Updates-1-15-2019/td-p/10877581" target="_blank">All Patch Notes (Xb1)</a></h1>');
+    $('body').append('<h1 class=""><a href="https://community.callofduty.com/t5/Call-of-Duty-Black-Ops-4/Call-of-Duty-Black-Ops-4-PS4-Updates-1-15-2019/m-p/10877580/thread-id/2701" target="_blank">All Patch Notes (PS4)</a></h1>');
+    $("body").append('<button class="button">Home</button>');
+});
+$('.button').click(function() {
+    location.reload();
+});
